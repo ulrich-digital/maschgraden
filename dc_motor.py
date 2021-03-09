be7b4af5d7a77a46a9b39befa426f717ae89ceb9
Raspi <matthias@ulrich.digital>
2021-03-09 14:21:33 +0100

    test

--- dc_motor.py
+++ dc_motor.py
@@ -10,9 +10,10 @@ kit = MotorKit(i2c=board.I2C())
  
 kit.motor1.throttle = 1.0
 time.sleep(1)
-kit.motor1.throttle = 0.0
-time.sleep(5)
 kit.motor1.throttle = 0.5
+time.sleep(1)
+kit.motor1.throttle = -1.0
+time.sleep(1)
 kit.motor1.throttle = 0.0
 
 exit()
