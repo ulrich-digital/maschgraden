aba498f5ddae0c3e92b9530bccb50d33ef096225
Matthias Ulrich <post@ulrich.digital>
2021-03-09 13:44:33 +0100

    Update dc_motor.py

--- dc_motor.py
+++ dc_motor.py
@@ -1,10 +1,28 @@
+## Sytem (Tastendruck usw.)
+import sys
+import termios
+import tty
+inkey_buffer = 1
+
+## Adafruit
 import time
 import board
 from adafruit_motorkit import MotorKit
+ 
+##
+while 1:
+    key = (inkey())
+    if key in ['w','a','s','d']:
+        print key
+        kit.motor1.throttle = 0.0
+
+    if key == 'q':
+        exit()
+      
  
 kit = MotorKit(i2c=board.I2C())
  
 kit.motor1.throttle = 1.0
 time.sleep(1)
-kit.motor1.throttle = 1.0
+kit.motor1.throttle = 10.0
 kit.motor1.throttle = 0.0
