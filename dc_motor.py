9f40ece9442e9f8dea9a5bb853c15a9adfad95ec
Matthias Ulrich <post@ulrich.digital>
2021-03-09 13:40:10 +0100

    Update dc_motor.py

--- dc_motor.py
+++ dc_motor.py
@@ -5,5 +5,6 @@ from adafruit_motorkit import MotorKit
 kit = MotorKit(i2c=board.I2C())
  
 kit.motor1.throttle = 1.0
-time.sleep(0.5)
+time.sleep(1)
 kit.motor1.throttle = 1.0
+kit.motor1.throttle = 0.0
